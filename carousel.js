4a8e5698a9653d89b08fe947cb6aeb14c2b036e6
Karoly <hornyal.mobile@gmail.com>
2022-12-10 21:10:06 +0000

    description update

--- carousel.js
+++ carousel.js
@@ -4,8 +4,8 @@ const slidingTime = config.slidingTime;
 const timeGap = config.timeGap;
 const numOfSlides = config.numOfSlides;
 
-let currentLayout = numOfSlides;    //Number of the possible layout is equal to the number of slides/filses.
-let images = [];          //List of images, will be filled up automatically with the file names later.
+let currentLayout = numOfSlides;    //Initial value is the number of the slides/filses.
+let images = [];                    //List of images, will be filled up automatically with the file names later.
 
 // * Building the carousel * //
 const carousel = document.querySelector(".carousel");
@@ -86,25 +86,19 @@ function forward() {
    * Remember: Initially the dispayed slide is the second slide in the queue.
    * 1. Click is suspended while sliding is in progress, nothing happens when clicking on the control buttons.
    * 2. Click is enabled again when sliding has finished.
-   * 3. Set the z-index of the first slide of the queue to -50, and the others to 50. 
-   * 4. Increase the index of the displayed slide by 1, however,
-   *    if the displayed slide is the last slide of the initial queue,
-   *    then set the displayed index back to zero.
+   * 3. Set the z-index of the first slide to -50, and the others to 50. 
+   * 4. Increase the indexOfDisplayed by 1, however, if it is the last index, then set it to 0.
    * 5. Synchronize the indicator to the displayed slide.
-   * 6. Jump the first slide of the queue to the last position, then slide the whole queue to the left. 
-   * 7. Decrease the current layout by 1, however,
-   *    if it is 1, then set it to the value of the number of the slides.
+   * 6. Move the first slide to the last position, also move all other slides to left. 
+   * 7. Decrease the current layout by 1, however, if it is 1, then set it back to the initial value.
    */
-
   if (!isClickSuspended) {
     //1.
     isClickSuspended = true;
-
     //2.
     setTimeout(() => {
       isClickSuspended = false;
     }, slidingTime);
-
     //3.
     slides.forEach((slide, i) => {
       if (i === indexOfDisplayed) {
@@ -113,13 +107,10 @@ function forward() {
         slide.style.zIndex = '50';
       }
     });
-
     //4.
     indexOfDisplayed === numOfSlides - 1 ? (indexOfDisplayed = 0) : indexOfDisplayed++;
-
     //5.
     setIndicator();
-
     //6.
     slides.forEach((slide, i) => {
       if (currentLayout === numOfSlides - i) {
@@ -129,32 +120,42 @@ function forward() {
         slide.style.left = `${newLeft}%`;
       }
     });
-
     //7.
     currentLayout === 1 ? (currentLayout = numOfSlides) : currentLayout--;
   }
 }
 
 function backward() {
+  /**
+   * Remember: Initially the dispayed slide is the second slide in the queue.
+   * 1. Click is suspended while sliding is in progress, nothing happens when clicking on the control buttons.
+   * 2. Click is enabled again when sliding has finished.
+   * 3. Set the z-index of the first and last slide to -50, and the others to 50. 
+   * 4. Decrease the indexOfDisplayed by 1, however, if it is 0, then set it to the last index.
+   * 5. Synchronize the indicator to the displayed slide.
+   * 6. ******** 
+   * 7. Move the last slide to the first position, also move all other slides to right. 
+   * 8. Increase the current layout by 1, however, if it is the initial value, then set it to 1.
+   */
   if (!isClickSuspended) {
+    //1.
     isClickSuspended = true;
-
+    //2.
     setTimeout(() => {
       isClickSuspended = false;
     }, slidingTime);
-
+    //3.
     slides.forEach((slide, i) => {
       slide.style.zIndex = '50';
     });
-
     indexOfDisplayed === 0
-      ? (slides[numOfSlides - indexOfDisplayed - 1].style.zIndex = "-50")
+      ? (slides[numOfSlides - 1].style.zIndex = "-50")
       : (slides[indexOfDisplayed - 1].style.zIndex = "-50");
-
+    //4.
     indexOfDisplayed === 0 ? (indexOfDisplayed = numOfSlides - 1) : indexOfDisplayed--;
-
+    //5.
     setIndicator();
-
+    //6.
     for (let i = 0; i < numOfSlides; i++) {
       for (let j = 0; j < numOfSlides - 1; j++) {
         if (currentLayout === numOfSlides - j - 1) {
@@ -165,26 +166,26 @@ function backward() {
         }
       }
     }
-
+    //7.
     slides.forEach((slide, i) => {
       let newLeft = Number(slide.style.left.split('%').join('')) + 100;
       slide.style.left = `${newLeft}%`;
     });
-
+    //8.
     currentLayout === numOfSlides ? (currentLayout = 1) : currentLayout++;
   }
 }
 
+// * Setting the click event of the control buttons  * //
 const btnLeft = document.querySelector(".btnLeft");
 const btnRight = document.querySelector(".btnRight");
-
 btnLeft.addEventListener("click", () => {
   backward();
 });
-
 btnRight.addEventListener("click", () => {
   forward();
 });
+// *** Setting the click event of the control buttons  *** //
 
 function autoRun() {
   /**
@@ -194,43 +195,37 @@ function autoRun() {
    * 4. Sliding stoppes when windows is not active.
    * 5. Sliding starts again when window is active again.
    */
-
   //1.
   let autoRun = setInterval(() => {
     forward();
   }, timeGap + slidingTime);
-  //console.log(autoRun, "STARTED");
-
+  console.log(autoRun, "STARTED");
   let id = autoRun;
-
   //2.
   carousel.addEventListener("mouseover", () => {
     clearInterval(id);
-    //console.log(id, "STOPPED");
+    console.log(id, "STOPPED");
   });
-
   //3.
   carousel.addEventListener("mouseout", () => {
     let runAgain = setInterval(() => {
       forward();
     }, timeGap + slidingTime);
     id = runAgain;
-    //console.log(id, "STARTED");
+    console.log(id, "STARTED");
   });
-
   //4.
   window.addEventListener("blur", () => {
     clearInterval(id);
-    //console.log(id, "STOPPED");
+    console.log(id, "STOPPED");
   });
-
   //5.
   window.addEventListener("focus", () => {
     let runAgain = setInterval(() => {
       forward();
     }, timeGap + slidingTime);
     id = runAgain;
-    //console.log(id, "STARTED");
+    console.log(id, "STARTED");
   });
 }
 
